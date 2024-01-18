1b40e2dbd387e79bb12b881134b7e1bbc6478ff7
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2024-01-18 10:49:36 +0000

    Deploying to gh-pages from @ GeorgiySergeev/goit-react-hw-08-phonebook@d40ecd0e0b26998b747dbcd732227c8114ba5432 🚀

---
+++ static/js/373.77b64d84.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[373],{4373:function(t,e,c){c.d(e,{hjJ:function(){return r}});var a=c(9983);function r(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M294.1 256L167 129c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.3 34 0L345 239c9.1 9.1 9.3 23.7.7 33.1L201.1 417c-4.7 4.7-10.9 7-17 7s-12.3-2.3-17-7c-9.4-9.4-9.4-24.6 0-33.9l127-127.1z"}}]})(t)}}}]);
+//# sourceMappingURL=373.77b64d84.chunk.js.map
