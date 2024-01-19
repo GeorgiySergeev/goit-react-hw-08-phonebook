b51554915768634f672945d7530c5cfdf0eb6675
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2024-01-19 22:46:54 +0200

    Update GoToAppLink.jsx

--- src/components/GoToAppLink/GoToAppLink.jsx
+++ src/components/GoToAppLink/GoToAppLink.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectIsLogedIn } from '../../redux/auth/auth-selectors';
 
 import { LinkContainer, StyledArrow } from './GoToAppLink.styled';
 
 const GoToAppLink = () => {
+  const isLoggedIn = useSelector(selectIsLogedIn);
+
   return (
     <LinkContainer>
       <Link to="/contacts" style={{ color: '#E05160', zIndex: 99 }}>
-        Go to App
+        {!isLoggedIn ? 'Let`s start' : 'My contacts'}
       </Link>
       <StyledArrow />
     </LinkContainer>
